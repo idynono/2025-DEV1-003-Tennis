21737b7f0dae4984d320e68656156830035edc32
nidy <idykaya@gmail.com>
2025-04-02 15:57:11 +0200

    add test to intitialize an game and score point

--- src/test/java/com/bnp/assignement/tennis/TennisApplicationTests.java
+++ src/test/java/com/bnp/assignement/tennis/TennisApplicationTests.java
@@ -10,4 +10,26 @@ class TennisApplicationTests {
     void contextLoads() {
     }
 
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void initialScoreIsZeroZero() {
+        assertEquals("0-0", game.getScore());
+    }
+
+    @Test
+    void player1ScoresOnce() {
+        game.player1Scores();
+        assertEquals("15-0", game.getScore());
+    }
+
+    @Test
+    void player2ScoresOnce() {
+        game.player2Scores();
+        assertEquals("0-15", game.getScore());
+    }
+
 }
